a71937afc44a81149b262abc78290f6818264c3a
Emily <emilyliu7321@gmail.com>
2023-10-11 15:17:08 -0700

    Update folder to include ts and example.env name

---
+++ ts-bot/index.js
@@ -0,0 +1,46 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const api_1 = require("@atproto/api");
+const dotenv = __importStar(require("dotenv"));
+const cron_1 = require("cron");
+const process = __importStar(require("process"));
+dotenv.config();
+// Create a Bluesky Agent 
+const agent = new api_1.BskyAgent({
+    service: 'https://bsky.social',
+});
+async function main() {
+    await agent.login({ identifier: process.env.BLUESKY_USERNAME, password: process.env.BLUESKY_PASSWORD });
+    const response = await agent.post({
+        text: "🙂"
+    });
+}
+main();
+// Run this on a cron job
+const scheduleExpressionMinute = '* * * * *'; // Run once every minute for testing
+const scheduleExpression = '0 */3 * * *'; // Run once every three hours in prod
+const job = new cron_1.CronJob(scheduleExpressionMinute, main);
+job.start();
---
+++ ts-bot/index.ts
@@ -0,0 +1,31 @@
+import { BskyAgent } from '@atproto/api';
+import * as dotenv from 'dotenv';
+import { CronJob } from 'cron';
+import * as process from 'process';
+
+dotenv.config();
+
+// Create a Bluesky Agent 
+const agent = new BskyAgent({
+    service: 'https://bsky.social',
+  })
+
+
+async function main() {
+    await agent.login({ identifier: process.env.BLUESKY_USERNAME!, password: process.env.BLUESKY_PASSWORD!})
+    await agent.post({
+        text: "🙂"
+    });
+    console.log("Just posted!")
+}
+
+main();
+
+
+// Run this on a cron job
+const scheduleExpressionMinute = '* * * * *'; // Run once every minute for testing
+const scheduleExpression = '0 */3 * * *'; // Run once every three hours in prod
+
+const job = new CronJob(scheduleExpression, main); // change to scheduleExpressionMinute for testing
+
+job.start();
