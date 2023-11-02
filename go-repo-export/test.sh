4756a66da09ae6f2fa066962d0534c483d169b3b
bryan newbold <bnewbold@robocracy.org>
2023-11-02 16:14:09 -0700

    go-export-repo: polish

---
+++ go-repo-export/test.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -o pipefail
+set -e
+set -u
+
+go build .
+./go-export-repo download atproto.com
+./go-export-repo list-records did:plc:ewvi7nxzyoun6zhxrhs64oiz.car
+./go-export-repo unpack did:plc:ewvi7nxzyoun6zhxrhs64oiz.car
+./go-export-repo list-blobs atproto.com
+./go-export-repo list-blobs did:plc:ewvi7nxzyoun6zhxrhs64oiz
+./go-export-repo download-blobs atproto.com
