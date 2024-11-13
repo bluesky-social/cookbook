75bda3aa2131721cdc0f6b7ebf4495ad9a7b76df
rottenmondays <tumstuffblr@gmail.com>
2024-11-12 17:57:31 -0800

    Delete unnecessary files

--- python-oauth-web-app/generate_jwk.py
+++
@@ -1,9 +0,0 @@
-import time
-from authlib.jose import JsonWebKey
-
-if __name__ == "__main__":
-    now = int(time.time())
-    key = JsonWebKey.generate_key(
-        "EC", "P-256", options={"kid": f"demo-{now}"}, is_private=True
-    )
-    print(key.as_json(is_private=True))
