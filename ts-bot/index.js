a4341855ea13becb68d7df60e609645d9020be3d
weebney <weebney@gmail.com>
2024-12-06 11:41:48 -0500

    modernize and simplify `ts-bot`

--- ts-bot/index.js
+++ ts-bot/index.js
@@ -1,60 +1,28 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || (function () {
-    var ownKeys = function(o) {
-        ownKeys = Object.getOwnPropertyNames || function (o) {
-            var ar = [];
-            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
-            return ar;
-        };
-        return ownKeys(o);
-    };
-    return function (mod) {
-        if (mod && mod.__esModule) return mod;
-        var result = {};
-        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
-        __setModuleDefault(result, mod);
-        return result;
-    };
-})();
 Object.defineProperty(exports, "__esModule", { value: true });
 const api_1 = require("@atproto/api");
-const dotenv = __importStar(require("dotenv"));
-const cron_1 = require("cron");
-const process = __importStar(require("process"));
-dotenv.config();
-// Create a Bluesky Agent
+require("node:process");
+// Create an AT Protocol Agent
 const agent = new api_1.AtpAgent({
     service: "https://bsky.social",
 });
 async function main() {
-    await agent.post({
-        text: "🙂",
-    });
-    await agent.login({
-        identifier: process.env.BLUESKY_USERNAME,
-        password: process.env.BLUESKY_PASSWORD,
-    });
-    console.log("Just posted!");
+    try {
+        // We just need to log in...
+        await agent.login({
+            identifier: process.env.BLUESKY_HANDLE,
+            password: process.env.BLUESKY_PASSWORD,
+        });
+        console.log("Successfully logged in!");
+        // Now we can make our post!
+        await agent.post({
+            text: "Hello, world! 🦋",
+        });
+        console.log("Successfully posted!");
+        // Et voilà!
+    }
+    catch (err) {
+        console.error("Uh oh! %s", err.message);
+    }
 }
 main();
-// Run this on a cron job
-const scheduleExpressionMinute = "* * * * *"; // Run once every minute for testing
-const scheduleExpression = "0 */3 * * *"; // Run once every three hours in prod
-const job = new cron_1.CronJob(scheduleExpression, main); // change to scheduleExpressionMinute for testing
-job.start();
--- ts-bot/index.ts
+++ ts-bot/index.ts
@@ -1,32 +1,29 @@
 import { AtpAgent } from "@atproto/api";
-import * as dotenv from "dotenv";
-import { CronJob } from "cron";
-import * as process from "process";
+import "node:process";
 
-dotenv.config();
-
-// Create a Bluesky Agent
+// Create an AT Protocol Agent
 const agent = new AtpAgent({
   service: "https://bsky.social",
 });
 
 async function main() {
-  await agent.login({
-    identifier: process.env.BLUESKY_USERNAME!,
-    password: process.env.BLUESKY_PASSWORD!,
-  });
-  await agent.post({
-    text: "🙂",
-  });
-  console.log("Just posted!");
+  try {
+    // We just need to log in...
+    await agent.login({
+      identifier: process.env.BLUESKY_HANDLE!,
+      password: process.env.BLUESKY_PASSWORD!,
+    });
+    console.log("Successfully logged in!");
+
+    // Now we can make our post!
+    await agent.post({
+      text: "Hello, world! 🦋",
+    });
+    console.log("Successfully posted!");
+    // Et voilà!
+  } catch (err: any) {
+    console.error("Uh oh! %s", err.message);
+  }
 }
 
 main();
-
-// Run this on a cron job
-const scheduleExpressionMinute = "* * * * *"; // Run once every minute for testing
-const scheduleExpression = "0 */3 * * *"; // Run once every three hours in prod
-
-const job = new CronJob(scheduleExpression, main); // change to scheduleExpressionMinute for testing
-
-job.start();
