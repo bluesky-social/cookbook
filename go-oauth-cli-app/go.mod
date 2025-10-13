8e02318dcee38785bec60b78bcfea3ad60ae9562
David Buchanan <david@blueskyweb.xyz>
2025-10-13 15:30:14 +0100

    go-oauth-cli-app: persist sessions

--- go-oauth-cli-app/go.mod
+++ go-oauth-cli-app/go.mod
@@ -8,12 +8,16 @@ require (
 )
 
 require (
+	github.com/adrg/xdg v0.5.3 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/earthboundkid/versioninfo/v2 v2.24.1 // indirect
 	github.com/golang-jwt/jwt/v5 v5.2.2 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/hashicorp/golang-lru/v2 v2.0.7 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/mattn/go-sqlite3 v1.14.22 // indirect
 	github.com/matttproud/golang_protobuf_extensions/v2 v2.0.0 // indirect
 	github.com/mr-tron/base58 v1.2.0 // indirect
 	github.com/prometheus/client_golang v1.17.0 // indirect
@@ -23,7 +27,10 @@ require (
 	gitlab.com/yawning/secp256k1-voi v0.0.0-20230925100816-f2616030848b // indirect
 	gitlab.com/yawning/tuplehash v0.0.0-20230713102510-df83abbf9a02 // indirect
 	golang.org/x/crypto v0.21.0 // indirect
-	golang.org/x/sys v0.22.0 // indirect
+	golang.org/x/sys v0.26.0 // indirect
+	golang.org/x/text v0.20.0 // indirect
 	golang.org/x/time v0.3.0 // indirect
 	google.golang.org/protobuf v1.33.0 // indirect
+	gorm.io/driver/sqlite v1.6.0 // indirect
+	gorm.io/gorm v1.31.0 // indirect
 )
--- go-oauth-cli-app/main.go
+++ go-oauth-cli-app/main.go
@@ -9,12 +9,15 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"path/filepath"
+	"time"
 
+	"github.com/adrg/xdg"
 	"github.com/bluesky-social/indigo/atproto/auth/oauth"
 	"github.com/urfave/cli/v3"
 )
 
-// returns immediately, returning the now-listening port number.
+// listens in the background and immediately returns the now-listening port number.
 // When the callback endpoint is requested, it returns the query parameters to the passed channel, and then shuts itself down
 func listenForCallback(ctx context.Context, res chan url.Values) (int, error) {
 	listener, err := net.Listen("tcp", ":0") // next available port
@@ -31,8 +34,9 @@ func listenForCallback(ctx context.Context, res chan url.Values) (int, error) {
 		res <- r.URL.Query()
 		w.Header().Set("Content-Type", "text/html; charset=utf-8")
 		w.WriteHeader(200)
-		w.Write([]byte("<!DOCTYPE html><html><body onload='close();'>OK</body></html>\n")) // close the webpage
-		go server.Shutdown(ctx)
+		// enough HTML to immediately close the webpage
+		w.Write([]byte("<!DOCTYPE html><html><body onload='close();'>OK</body></html>\n"))
+		go server.Shutdown(ctx) // XXX: is this the right context to pass?
 	})
 
 	go func() {
@@ -45,12 +49,46 @@ func listenForCallback(ctx context.Context, res chan url.Values) (int, error) {
 	return listener.Addr().(*net.TCPAddr).Port, nil
 }
 
-func main() {
+// Follows XDG conventions and creates the directories if necessary.
+// By default, on linux, this will be "~/.local/share/go-oauth-cli-app/oauth_sessions.sqlite3"
+func prepareDbPath() (string, error) {
+	appDataDir := filepath.Join(xdg.DataHome, "go-oauth-cli-app")
+
+	if err := os.MkdirAll(appDataDir, 0o700); err != nil {
+		return "", fmt.Errorf("failed to create app data directory: %w", err)
+	}
+
+	return filepath.Join(appDataDir, "oauth_sessions.sqlite3"), nil
+}
+
+func buildOAuthClient() (*oauth.ClientConfig, *oauth.ClientApp, *SqliteStore, error) {
 	config := oauth.ClientConfig{
 		ClientID:  "https://retr0.id/stuff/go-oauth-cli-app.json",
 		Scopes:    []string{"atproto", "repo:app.bsky.feed.post?action=create"},
 		UserAgent: "go-oauth-cli-app example",
 	}
+
+	dbPath, err := prepareDbPath()
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	store, err := NewSqliteStore(&SqliteStoreConfig{
+		DatabasePath:              dbPath,
+		SessionExpiryDuration:     time.Hour * 24 * 90,
+		SessionInactivityDuration: time.Hour * 24 * 14,
+		AuthRequestExpiryDuration: time.Minute * 30,
+	})
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	oauthClient := oauth.NewClientApp(&config, store)
+
+	return &config, oauthClient, store, nil
+}
+
+func main() {
 
 	cmd := &cli.Command{
 		Commands: []*cli.Command{
@@ -63,6 +101,11 @@ func main() {
 						return fmt.Errorf("identifier required")
 					}
 
+					config, oauthClient, _, err := buildOAuthClient()
+					if err != nil {
+						return err
+					}
+
 					callbackRes := make(chan url.Values, 1)
 					listenPort, err := listenForCallback(ctx, callbackRes)
 					if err != nil {
@@ -72,12 +115,12 @@ func main() {
 					fmt.Println("listening on", listenPort)
 					config.CallbackURL = fmt.Sprintf("http://127.0.0.1:%d/callback", listenPort)
 
-					oauthClient := oauth.NewClientApp(&config, oauth.NewMemStore()) // TODO: sqlite store in ~/.config/ or similar
 					authUrl, err := oauthClient.StartAuthFlow(ctx, identifier)
 					if err != nil {
 						return fmt.Errorf("logging in: %w", err)
 					}
 					fmt.Printf("authUrl: %s\n", authUrl)
+					// TODO: auto-open in browser
 
 					session, err := oauthClient.ProcessCallback(ctx, <-callbackRes)
 					if err != nil {
@@ -85,6 +128,40 @@ func main() {
 					}
 
 					fmt.Println("logged in:", session.AccountDID)
+
+					return nil
+				},
+			},
+			{
+				Name:  "status",
+				Usage: "print information about the current OAuth session, if one exists",
+				Action: func(ctx context.Context, cmd *cli.Command) error {
+					_, oauthClient, store, err := buildOAuthClient()
+					if err != nil {
+						return err
+					}
+
+					lastSessData, err := store.GetMostRecentSession(ctx)
+					if err != nil {
+						return err
+					}
+
+					oauthSess, err := oauthClient.ResumeSession(ctx, lastSessData.AccountDID, lastSessData.SessionID)
+					if err != nil {
+						return err
+					}
+
+					c := oauthSess.APIClient()
+					var resp struct {
+						Handle string `json:"handle"`
+					}
+					if err := c.Get(ctx, "com.atproto.server.getSession", nil, &resp); err != nil {
+						return err
+					}
+
+					fmt.Println("did:   ", oauthSess.Data.AccountDID)
+					fmt.Println("handle:", resp.Handle)
+					fmt.Println("host:  ", oauthSess.Data.HostURL)
 
 					return nil
 				},
